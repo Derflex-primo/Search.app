5fd4b792210341ca45f6029eee511da78564aaa0
Derflex-primo <dryldeogracias@gmail.com>
2023-07-06 22:56:00 +0800

    initialized git-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import MovieCart from "./MovieCart";
+import SearchIcon from "./search.svg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=7093b65d"
+
+ 
+const movie1 = {
+    
+        "Title": "Hulk",
+        "Year": "2003",
+        "imdbID": "tt0286716",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMzQwZDg1MGEtN2E5My00ZDJlLWI4MzItM2U2MjJhYzlkNmEzXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_SX300.jpg"
+    
 }
+ 
 
-export default App;
+export default function App() {
+
+    const [movies, setMovies] =  useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+   
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search)
+    }
+
+
+    useEffect(() => {{
+     searchMovies("dog")
+
+    }},[])
+    return (
+        <div className="app">
+           <h1>YourMovies</h1>
+           <div className="search">
+            <input placeholder="Look for it here..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}></input>
+            <img src={SearchIcon} alt="search"
+            onClick={() => searchMovies(searchTerm)}></img>
+           </div>
+
+           {movies?.length > 0
+            ? (
+            <div className="container">
+                {movies.map((movie) => 
+                (<MovieCart movie={movie} />))}  
+            </div>): 
+             (
+            <div className="empty"> 
+            <h1>No movies Found!</h1>
+            </div>
+            )}
+          
+
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// api_key=59a2cc2fa5db4670eabfee80c179fa9a"
+// https://api.themoviedb.org/3 
+ReactDOM.render(<App />, document.getElementById("root"));
